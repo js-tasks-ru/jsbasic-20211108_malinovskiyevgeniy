fceda3104003d3170b7d21eebe213c0c8e57ddd7
Malinovskiy Genya <malinovskiygenya@1s-MacBook-Pro.local>
2021-11-17 14:13:13 +0800

    Name check

--- 1-module/2-task/index.js
+++ 1-module/2-task/index.js
@@ -1,24 +1,24 @@
-/**
- * Эту функцию трогать не нужно
- */
 function print(text) {
-  console.log(text);
-}
-
-/**
- * Эту функцию нужно поменять так,
- * чтобы функция sayHello работала корректно
- */
-function isValid(name) {
-  // ваш код...
-}
-
-function sayHello() {
-  let userName = prompt('Введите ваше имя');
-
-  if (isValid(userName)) {
-    print(`Welcome back, ${userName}!`);
-  } else {
-    print('Некорректное имя');
+    console.log(text);
   }
-}
+  
+function isValid(name) {
+  if (!name || name.length < 4 || name.includes(' ')) {
+      return (false);
+  }
+  else return (true);
+} 
+
+  
+function sayHello() {
+    let userName = prompt('Введите ваше имя');
+  
+    if (isValid(userName)) {
+      print(`Welcome back, ${userName}!`);
+    } else {
+      print('Некорректное имя');
+    }
+  }
+  
+  sayHello();
+  
